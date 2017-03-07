a429a34afa1e7ccdc1239ef2fe0942c8086139ca
Stan <stann@live.nl>
2017-03-07 10:43:01 +0100

    Basic JPA implementation

---
+++ src/main/java/com/s63b/Car.java
@@ -0,0 +1,33 @@
+package com.s63b;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+
+    private String id;
+    private String licensePlate;
+
+    public Car(){}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLicensePlate(){
+        return this.licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate){
+        this.licensePlate = licensePlate;
+    }
+}
---
+++ src/main/java/com/s63b/JPATest.java
@@ -0,0 +1,46 @@
+package com.s63b;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import java.util.List;
+
+@RestController
+public class JPATest {
+
+    private static EntityManagerFactory factory;
+    private static EntityManager em;
+
+    public JPATest(){
+        factory = Persistence.createEntityManagerFactory("HibernatePersistenceUnit");
+        em = factory.createEntityManager();
+
+        em.getTransaction().begin();
+        Car car = new Car();
+        car.setLicensePlate("ASDF");
+
+        em.persist(car);
+        em.getTransaction().commit();
+    }
+
+    @RequestMapping("/jpa")
+    public String init() {
+        Query q = em.createQuery("SELECT c FROM Car c");
+        List<Car> carList = q.getResultList();
+
+        String s = "";
+
+        for (Car car : carList) {
+            s += " [" + car.getLicensePlate() + "] ";
+        }
+
+        s += " -- Size: " + carList.size();
+
+        return s;
+    }
+}
