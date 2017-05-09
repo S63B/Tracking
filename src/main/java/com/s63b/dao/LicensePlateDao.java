0ce80efdefd06d5ee7b2f12c25fa07f9275d2bdb
Stan <stann@live.nl>
2017-05-09 09:30:55 +0200

    Using licenseplates again.

---
+++ src/main/java/com/s63b/dao/LicensePlateDao.java
@@ -0,0 +1,7 @@
+package com.s63b.dao;
+
+import com.S63B.domain.Entities.LicensePlate;
+
+public class LicensePlateDao extends BaseDao<LicensePlate> {
+
+}
--- src/main/java/com/s63b/dao/PolDao.java
+++ src/main/java/com/s63b/dao/PolDao.java
@@ -2,22 +2,13 @@ package com.s63b.dao;
 
 import com.S63B.domain.Entities.Pol;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
 import java.util.List;
 
-public class PolDao {
-    private EntityManager em;
+public class PolDao extends BaseDao<Pol> {
 
-    public PolDao() {
-        em = Persistence.createEntityManagerFactory("HibernatePersistenceUnit").createEntityManager();
-    }
-
-    public boolean addPol(Pol pol){
+    public boolean deletePols(String licensePlate){
         try{
-            em.getTransaction().begin();
-            em.persist(pol);
-            em.getTransaction().commit();
+            em.createNamedQuery("Pol.deletePolls", Pol.class).setParameter("licensePlate", licensePlate);
             return true;
         }catch (Exception e){
             e.printStackTrace();
@@ -25,28 +16,18 @@ public class PolDao {
         }
     }
 
-    public boolean deletePols(int trackerID){
+    public List<Pol> getPols(String licensePlate){
         try{
-            em.createNamedQuery("Pol.deletePolls", Pol.class).setParameter("trackerID", trackerID);
-            return true;
-        }catch (Exception e){
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    public List<Pol> getPols(int trackerID){
-        try{
-            return em.createNamedQuery("Pol.getPolls", Pol.class).setParameter("trackerID", trackerID).getResultList();
+            return em.createNamedQuery("Pol.getPolls", Pol.class).setParameter("licensePlate", licensePlate).getResultList();
         }catch (Exception e){
             e.printStackTrace();
             return null;
         }
     }
 
-    public List<Pol> getPolsBetween(int trackerID, long startDate, long endDate){
+    public List<Pol> getPolsBetween(String licensePlate, long startDate, long endDate){
         try{
-            return em.createNamedQuery("Pol.getPollsBetween", Pol.class).setParameter("trackerID", trackerID).setParameter("startDate", startDate).setParameter("endDate", endDate).getResultList();
+            return em.createNamedQuery("Pol.getPollsBetween", Pol.class).setParameter("licensePlate", licensePlate).setParameter("startDate", startDate).setParameter("endDate", endDate).getResultList();
         }catch (Exception e){
             e.printStackTrace();
             return null;
